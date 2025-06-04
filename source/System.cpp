0330a46197a5918d22a1576428a1266d44b72cb1
Byter64 <50293046+Byter64@users.noreply.github.com>
2025-06-04 17:27:08 +0200

    addendum

--- source/System.cpp
+++ source/System.cpp
@@ -7,7 +7,7 @@ volatile unsigned int* SYSTEM_TIME_2 = (volatile unsigned int*)(SYSTEM_TIME + 8)
 volatile unsigned int* SYSTEM_TIME_3 = (volatile unsigned int*)(SYSTEM_TIME + 12);
 volatile unsigned int* SYSTEM_TIME_4 = (volatile unsigned int*)(SYSTEM_TIME + 16);//Most precise
 
-volatile void* CONTROLLER_START = (volatile void*)0x02000200;
+volatile char* CONTROLLER_START = (volatile void*)0x02000200;
 volatile unsigned short* CONTROLLER_0 = (volatile unsigned short*)(CONTROLLER_START + 0);
 volatile unsigned short* CONTROLLER_1 = (volatile unsigned short*)(CONTROLLER_START + 2);
 
