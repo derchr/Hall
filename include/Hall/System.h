a771d2099c35bab71b054f0d58009c643f54e91e
Byter64 <50293046+Byter64@users.noreply.github.com>
2025-06-05 16:32:56 +0200

    Refactored SystemTime

--- include/Hall/System.h
+++ include/Hall/System.h
@@ -8,11 +8,10 @@ namespace Hall
 	static void* const SD_CARD_START = (void* const) 0x80000000; //Start of the SD-Card
 
 	/// <summary>
-	/// Returns a 4-Byte excerpt of the 8-Byte system time in ticks since startup. The precision determines the byte offset.
+	/// Returns an 8-Byte int, that represents the number of ticks that passed since the system's boot-up. Divide by SYSTEM_CLK_FREQUENCY to get seconds
 	/// </summary>
-	/// <param name="precision">Value in [0, 4]. The higher the precision, the varier you must be because of overflows</param>
-	/// <returns>The system clock with the given precision</returns>
-	unsigned int GetSystemTime(int precision);
+	/// <returns>The system time since boot-up in ticks</returns>
+	unsigned long long GetSystemTime();
 
 	/// <summary>
 	/// Returns the state of all buttons on the controller.
@@ -21,6 +20,7 @@ namespace Hall
 	/// <returns>Each button corresponds to one bit: 0 - B, 1 - Y, 2 - Select, 3 - Start, 4 - Up, 5 - Down, 6 - Left, 7 - Right, 8 - A, 9 - X, 10 - L, 11 - R</returns>
 	unsigned short GetController(int id);
 
+	unsigned int GetSystemTimeExcerpt(int precision);
 	bool GetA(unsigned short controller);
 	bool GetB(unsigned short controller);
 	bool GetX(unsigned short controller);
