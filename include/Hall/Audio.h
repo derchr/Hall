ddb23d2d94aac5f7b20318f67289de593cc84c11
Byter64 <50293046+Byter64@users.noreply.github.com>
2025-06-10 16:37:22 +0200

    Added const to audio data

--- include/Hall/Audio.h
+++ include/Hall/Audio.h
@@ -15,7 +15,7 @@ namespace Hall
 	/// <param name="data">A pointer to the first sample of the audio data</param>
 	/// <param name="sampleCount">The total number of samples in the audio data</param>
 	/// <param name="volume">The channel's local volume. 0 is muted, 128 is the default, 255 is max</param>
-	void SetupMono(int channelID, short* data, int sampleCount, unsigned char volume = 128);
+	void SetupMono(int channelID, const short* data, int sampleCount, unsigned char volume = 128);
 	
 	/// <summary>
 	/// Prepares a channel to be used to play a looped single-channel audio
@@ -26,7 +26,7 @@ namespace Hall
 	/// <param name="loopStart">The index of the first sample of the loop (inclusive)</param>
 	/// <param name="loopEnd">The index of the last sample of the loop (exclusive?)</param>
 	/// <param name="volume">The channel's local volume. 0 is muted, 128 is the default, 255 is max</param>
-	void SetupMono(int channelID, short* data, int sampleCount, unsigned int loopStart, unsigned int loopEnd, unsigned char volume = 128);
+	void SetupMono(int channelID, const short* data, int sampleCount, unsigned int loopStart, unsigned int loopEnd, unsigned char volume = 128);
 	
 	/// <summary>
 	/// Prepares two channels to be used to play a non-looped stereo audio
@@ -36,7 +36,7 @@ namespace Hall
 	/// <param name="data">A pointer to the first sample of the audio data</param>
 	/// <param name="sampleCount">The total number of samples PER CHANNEL in the audio data</param>
 	/// <param name="volume">The channel's local volume. 0 is muted, 128 is the default, 255 is max</param>
-	void SetupStereo(int channelID_left, int channelID_right, short* data, int sampleCount, unsigned char volume = 128);
+	void SetupStereo(int channelID_left, int channelID_right, const short* data, int sampleCount, unsigned char volume = 128);
 
 	/// <summary>
 	/// Prepares two channels to be used to play a looped stereo audio
@@ -48,7 +48,7 @@ namespace Hall
 	/// <param name="loopStart">The index of the first sample of the loop (inclusive)</param>
 	/// <param name="loopEnd">The index of the last sample of the loop (exclusive?)</param>
 	/// <param name="volume">The channel's local volume. 0 is muted, 128 is the default, 255 is max</param>
-	void SetupStereo(int channelID_left, int channelID_right, short* data, int sampleCount, unsigned int loopStart, unsigned int loopEnd, unsigned char volume = 128);
+	void SetupStereo(int channelID_left, int channelID_right, const short* data, int sampleCount, unsigned int loopStart, unsigned int loopEnd, unsigned char volume = 128);
 	
 
 	void Play(unsigned char channelSelect);	
